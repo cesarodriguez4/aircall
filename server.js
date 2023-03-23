dfc6cca958e18039a4459a13a7e8f15d39175da4
Cesar Rodriguez <cesarodriguez4@gmail.com>
2023-03-22 23:38:59 -0400

    Date styling

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
-// All routes to /
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
